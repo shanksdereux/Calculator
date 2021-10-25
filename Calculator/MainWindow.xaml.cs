39539d2d324dc6d4f72740eb9993214fab0dd3a1
Dc0rLEonE <nikkiecabs548@gmail.com>
2021-10-25 14:15:33 +0800

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        string output = "", operation = "";
+
+        double temp = 0, outputTemp = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //this.DataContext = this;
+
+            DivideBtn.Content = "\u00F7";
+            SquareBtn.Content = "x" + "\u00B2";
+            SquareRootBtn.Content = "\u221A";
+        }
+
+        private void NumBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ((Button)sender).Name;
+
+            switch (name)
+            {
+                case "OneBtn":
+                    output += "1";
+                    OutputTextBlock.Text=output;
+                    break;
+
+                case "TwoBtn":
+                    output += "2";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "ThreeBtn":
+                    output += "3";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "FourBtn":
+                    output += "4";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "FiveBtn":
+                    output += "5";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "SixBtn":
+                    output += "6";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "SevenBtn":
+                    output += "7";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "EightBtn":
+                    output += "8";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "NineBtn":
+                    output += 9;
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "ZeroBtn":
+                    output += 0;
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "DecimalBtn":
+                    if (!output.Contains("."))
+                    {
+                        output += ".";
+                        OutputTextBlock.Text = output;
+                    }       
+                    break;
+            }
+        }
+
+        private void PlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Addition";
+            }
+        }
+
+        private void MinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "Minus";
+            }
+        }
+
+        private void TimesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Multiplication";
+            }
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            output = "";
+            OutputTextBlock.Text = output;
+        }
+
+   
+        private void DivideBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Division";
+            }
+        }
+
+        private void SquareRootBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+                output = Math.Sqrt(temp).ToString();
+                OutputTextBlock.Text = output;
+            }
+        }
+
+        private void SquareBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+                output = (temp * temp).ToString();
+                OutputTextBlock.Text = output;       
+            }
+        }
+
+        private void EqualsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (operation)
+            {
+                case "Addition":
+                    outputTemp = temp + double.Parse(output);
+                    output = outputTemp.ToString();
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "Minus":
+                    outputTemp = temp - double.Parse(output);
+                    output = outputTemp.ToString();
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "Multiplication":
+                    outputTemp = temp * double.Parse(output);
+                    output = outputTemp.ToString();
+                    OutputTextBlock.Text = output;
+                    break;
+                case "Division":
+                    outputTemp = temp / double.Parse(output);
+                    output = outputTemp.ToString();
+                    OutputTextBlock.Text = output;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
